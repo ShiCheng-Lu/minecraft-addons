c5227ee45cc7cae5302e97e7fb3c173045f7f6fe
ShiCheng Lu <shichenglu0708@gmail.com>
2022-12-06 22:33:31 -0500

    autogen manifest

--- scripts.js
+++ scripts.js
@@ -1,15 +1,27 @@
 const { execSync } = require("child_process")
+const { randomUUID } = require("crypto")
 const fse = require("fs-extra")
 
 function newProject(name) {
-    if (fse.existsSync(name)) {
-        return console.log("project already exist")
+    // if (fse.existsSync(name)) {
+    //     return console.log("project already exist")
+    // }
+    fse.copySync("template", `${name}`)
+
+    const uuidMap = {
+        "{{uuid-behavior}}": randomUUID(),
+        "{{uuid-behavior-module}}": randomUUID(),
+        "{{uuid-resource}}": randomUUID(),
+        "{{uuid-resource-module}}": randomUUID(),
     }
-    // copy default packs from bedrock-samples
-    fse.copy("bedrock-samples/behavior_pack", `${name}/behavior_pack`)
-    fse.copy("bedrock-samples/resource_pack", `${name}/resource_pack`)
-    // copy setup
-    fse.copy("template", `${name}`)
+
+    for (fileName of ["behavior_pack/manifest.json", "resource_pack/manifest.json"]) {
+        var file = fse.readFileSync(`${name}/${fileName}`, { encoding: 'utf-8' });
+        for (key in uuidMap) {
+            file = file.replace(key, uuidMap[key])
+        }
+        fse.writeFileSync(`${name}/${fileName}`, file, { encoding: 'utf-8' });
+    }
 }
 
 function main() {
