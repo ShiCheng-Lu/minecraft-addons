b0eafd8bd3b096e311ab003556dc961563d3918d
ShiCheng Lu <shichenglu0708@gmail.com>
2022-12-06 21:52:37 -0500

    add template for creating new project

--- scripts.js
+++ scripts.js
@@ -1,19 +1,39 @@
 const { execSync } = require("child_process")
+const fse = require("fs-extra")
+
+function newProject(name) {
+    if (existsSync(name)) {
+        return console.log("project already exist")
+    }
+    // copy default packs from bedrock-samples
+    fse.copySync("bedrock-samples/behavior_pack", `${name}/behavior_pack`)
+    fse.copySync("bedrock-samples/resource_pack", `${name}/resource_pack`)
+    // copy setup
+    fse.copySync("template", `${name}`)
+}
 
 function main() {
     const option = process.argv[2]
     const project = process.argv[3]
 
-    console.log("hello")
-
-    if (project === undefined || !["build", "package"].includes(option)) {
+    if (project === undefined || !["build", "package", "new"].includes(option)) {
         console.log(`
 Usage:
-    npm run <build|package> <project> [log]
+    npm run <option> <project> [log]
+
+    options: 
+        - build     build a project
+        - package   package a project
+        - new       create a new project
 `)
         return;
     }
 
+    if (option === "new") {
+        return newProject(project)
+    }
+
+    // command fallthrough
     var result = execSync(`cd ${project} & npm run ${option}`);
     if (process.argv[4] == "log") {
         console.log(result.toString())
